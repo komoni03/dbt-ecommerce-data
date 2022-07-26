6782f26a8e17a702662937ed8582ba7db5f38282
komoni <komoni03@gmail.com>
2022-07-26 10:01:45 +0100

    working on jinjas

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,5 +1,16 @@
+{%- set payments = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%} 
+
 with payments as (
     select * from {{ ref('stg_payments')}}
 )
 
-select * from payments
+select (
+    order_id,
+
+    {{% for payment in payments %}}
+        sum(CASE WHEN payment_method = '{{payment}}' THEN amount ELSE 0 END) as {{ payment }}_amount,
+    {{% endfor %}}
+)
+
+FROM payments
+GROUP BY order_id
