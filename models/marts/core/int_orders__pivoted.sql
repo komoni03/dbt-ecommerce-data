52cbd7d401ce6da53aef39dca1bbc75c12283dce
komoni <komoni03@gmail.com>
2022-07-26 10:05:06 +0100

    error changes

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,16 +1,17 @@
 {%- set payments = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%} 
 
 with payments as (
-    select * from {{ ref('stg_payments')}}
+    select * from {{ ref('stg_payments') }}
 )
 
 select (
     order_id,
 
-    {{% for payment in payments %}}
+    {% for payment in payments %}
         sum(CASE WHEN payment_method = '{{payment}}' THEN amount ELSE 0 END) as {{ payment }}_amount,
-    {{% endfor %}}
+    {% endfor %}
 )
 
 FROM payments
 GROUP BY order_id
+WHERE status = 'success'
